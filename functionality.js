a6c791f1d5133d90e4a53c84fbe957955758f0b4
Brandon Britt <BBRITT@augusta.edu>
2026-06-06 17:00:56 -0400

    Updated the Library's functionality and reorganized some of the stories.

--- functionality.js
+++ functionality.js
@@ -3,6 +3,7 @@
 // });
 const buttons = document.querySelectorAll(".sectionBtn");
 const title = document.getElementById("title");
+const blurbs = document.querySelectorAll(".descriptor");
 const stories = document.querySelectorAll(".stories");
 
 buttons.forEach(button => {
@@ -10,9 +11,14 @@ buttons.forEach(button => {
         const newTitle = button.dataset.title;
         title.textContent = newTitle;
 
+        blurbs.forEach(blurb => { blurb.classList.add("hidden"); });
         stories.forEach(section => { section.classList.add("hidden"); });
+
         const sectionID = button.dataset.section;
-        const visibleSection = document.getElementById(sectionID);
-        visibleSection.classList.remove("hidden");
+        document.getElementById(sectionID + "-desc").classList.remove("hidden");
+        document.getElementById(sectionID).classList.remove("hidden");
+
+        // const visibleSection = document.getElementById(sectionID);
+        // visibleSection.classList.remove("hidden");
     });
 });
